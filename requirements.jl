91bad59c9e71476744b69b0fea403e3835899168
Yaroslava Lochman <25887374+ylochman@users.noreply.github.com>
2025-03-11 10:31:54 +0100

    Update requirements.jl

--- requirements.jl
+++ requirements.jl
@@ -2,5 +2,5 @@ import Pkg
 
 add_packages = packages -> for pkg=packages Pkg.add(pkg) end
 
-add_packages(["Random", "LinearAlgebra", "Statistics"])
+add_packages(["LinearAlgebra", "Statistics"])
 add_packages(["JuMP", "SCS"])
